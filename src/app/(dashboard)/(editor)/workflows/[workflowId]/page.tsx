5a614e7e346542b89ab8eba35e65d8671963d357
amanraina1 <amanraina9086@gmail.com>
2025-10-22 01:11:19 +0530

    single worklow page ui partially done

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,11 +1,34 @@
+import {
+  Editor,
+  EditorError,
+  EditorLoading,
+} from "@/features/editor/components/editor";
+import { EditorHeader } from "@/features/editor/components/editor-header";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 interface PageParams {
   params: Promise<{
-    workflowId: String;
+    workflowId: string;
   }>;
 }
 
 const Page = async ({ params }: PageParams) => {
   const { workflowId } = await params;
-  return <p>Workflow ID: {workflowId}</p>;
+  prefetchWorkflow(workflowId);
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorError />}>
+        <Suspense fallback={<EditorLoading />}>
+          <EditorHeader workflowId={workflowId} />
+          <main className="flex-1">
+            <Editor workflowId={workflowId} />
+          </main>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 export default Page;
